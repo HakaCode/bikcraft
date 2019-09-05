35b2955996df4782f16fe76783fa26d35cd991cf
Haka Code / <willnegoo@gmail.com>
2019-09-05 04:24:12 -0300

    add quote
    adicionado o quote no footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+		<div class="quebra">
+			<blockquote class="quote-externo container">
+				<p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
+				<cite>WILLIAM MORRIS</cite>
+			</blockquote>
+		</div>
+
 		<footer>
 			<div class="footer">
 				<div class="container">
