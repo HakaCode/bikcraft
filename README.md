379cc563490d9166049c9f54a0484f3287a9c67d
Haka Code / <willnegoo@gmail.com>
2019-09-05 04:44:15 -0300

    deletado o quote da index

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bikcraft
  Projeto Bikcraft codado em Wordpress do Zero do Curso da Origamid. (Inicializado)
+
+![Image of Project]()
--- index.php
+++ index.php
@@ -87,10 +87,4 @@
 			</div>
 		</section>
 
-		<div class="quebra">
-			<blockquote class="quote-externo container">
-				<p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
-				<cite>WILLIAM MORRIS</cite>
-			</blockquote>
-		</div>
 
