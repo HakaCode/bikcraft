21488ebf735f20e941b64bc5187773ae9cda8eec
Haka Code / <willnegoo@gmail.com>
2019-09-06 17:27:40 -0300

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -6,12 +6,12 @@ get_header();
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //(if)se tiver post(s), (while)enquanto tiver post(s) mostra o post.?>
 		<section class="introducao">
 			<div class="container">
-				<h1>Bicicletas Feitas a Mão</h1>
+				<h1><?php the_field("titulo_introducao"); ?></h1>
 				<blockquote class="quote-externo">
-					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
-					<cite>WILLIAM MORRIS</cite>
+					<p><?php the_field("quote_introducao"); ?></p>
+					<cite><?php the_field("citacao_introducao"); ?></cite>
 				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
+				<a href="/bikcraft/produtos/" class="btn">Orçamento</a>
 			</div>
 		</section>
 		
@@ -46,8 +46,8 @@ get_header();
 			</ul>
 
 			<div class="call">
-				<p>clique aqui e veja os detalhes dos produtos</p>
-				<a href="produtos.html" class="btn btn-preto">Produtos</a>
+				<p><?php the_field("chamada_produtos"); ?></p>
+				<a href="/bikcraft/produtos/" class="btn btn-preto">Produtos</a>
 			</div>
 
 		</section>
@@ -56,14 +56,10 @@ get_header();
 		<section class="portfolio">
 			<div class="container">
 				<h2 class="subtitulo">Portfólio</h2>
-				<div class="portfolio_lista">
-					<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-					<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-					<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-				</div>
+					<?php include(TEMPLATEPATH . "/inc/clientes-portfolio.php"); // TEMPLATEPATH busca caminho para incluir informações de outras paginas que smp se repetem, "." no php é o mesmo que "somar".?>
 				<div class="call">
-					<p>conheça mais o nosso portfólio</p>
-					<a href="portfolio.html" class="btn">Portfólio</a>
+					<p><?php the_field("chamada_portfolio"); ?></p>
+					<a href="/bikcraft/portfolio/" class="btn">Portfólio</a>
 				</div>
 			</div>
 		</section>
