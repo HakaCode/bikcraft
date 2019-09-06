8d835825590dae1d9d8b35cc49f837bf8b949608
Haka Code / <willnegoo@gmail.com>
2019-09-05 23:15:27 -0300

    Add loop

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-	<p>Página Index</p>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	
+	<?php the_title(); ?>
+	<?php the_content(); ?>
+
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
