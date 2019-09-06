23f1ac880b7d4edfe6362274ffd93a70b69bb0bc
Haka Code / <willnegoo@gmail.com>
2019-09-05 23:29:47 -0300

    comments to remember
    Alguns comentarios para relembrar

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //(if)se tiver post(s), (while)enquanto tiver post(s) mostra o post.?>
 	
-	<?php the_title(); ?>
+	<h1><?php the_title(); ?></h1>
 	<?php the_content(); ?>
 
 <?php endwhile; else: ?>
