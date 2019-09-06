bc3dbf0ab3b985430f8b798783c0e9b5ec74a25c
Haka Code / <willnegoo@gmail.com>
2019-09-06 17:30:54 -0300

    add page to include
    Adicionado a pagina "clientes-portfolio" dentro da pasta "inc" para que seja incluido tanto na home quanto em multiplos lugares, se assim desejar.

---
+++ inc/clientes-portfolio.php
@@ -0,0 +1,15 @@
+<ul class="portfolio_lista rslides_portfolio">
+
+	<li>
+		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+		<div class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+	</li>
+
+	<li>
+		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+		<div class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+	</li>
+
+</ul>
