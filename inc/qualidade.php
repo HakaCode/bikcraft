0b29e1124ced71f0515d6984668d2b201f88ecf9
Haka Code / <willnegoo@gmail.com>
2019-09-06 22:21:52 -0300

    Update qualidade.php

--- inc/qualidade.php
+++ inc/qualidade.php
@@ -1,25 +1,21 @@
+<?php $sobre = get_page_by_title('sobre'); ?>
 <section class="qualidade container">
-	<h2 class="subtitulo">Qualidade</h2>
-	<img src="<?php echo get_template_directory_uri(); ?>/img/bikcraft-qualidade.png" alt="Bikcraft">
+	<h2 class="subtitulo"><?php the_field('titulo_qualidade', $sobre); //vai pegar o titulo qualidade que estiver dentro da pagina "sobre". ?></h2>
+	<img src="<?php the_field('logo_bikcraft', $sobre); ?>" alt="Bikcraft">
 	<ul class="qualidade_lista">
+		<?php if(have_rows('item_qualidade', $sobre)): while(have_rows('item_qualidade', $sobre)) : the_row(); ?>
 		<li class="grid-1-3">
-			<h3>Durabilidade</h3>
-			<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
+			<h3><?php the_sub_field('titulo_item_qualidade', $sobre); ?></h3>
+			<p><?php the_sub_field('descricao_item_qualidade', $sobre); ?></p>
 		</li>
-		<li class="grid-1-3">
-			<h3>Design</h3>
-			<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-		</li>
-		<li class="grid-1-3">
-			<h3>Sustentabilidade</h3>
-			<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-		</li>
+		<?php endwhile; else : endif; //primeiramente feche oq abriu mais recentemente ?>
+		
 	</ul>
-	<?php if (is_page('sobre')) { 
+	<?php if (!is_page('sobre')) { 
 		// se for(estiver na) pagina 'sobre' não mostrar o conteúdo na pagina "sobre.php" e podendo ser visivel em qualquer outra pagina. ?>
 	<div class="call">
-		<p>conheça mais a nossa história</p>
-		<a href="sobre.html" class="btn btn-preto">Sobre</a>
+		<p><?php the_field('chamada_sobre', $sobre); ?></p>
+		<a href="/bikcraft/sobre/" class="btn btn-preto">Sobre</a>
 	</div>
 	<?php } ?>
 </section>
